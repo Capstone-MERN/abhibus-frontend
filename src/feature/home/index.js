87f0f14ea78f2c431c64395803841a0ab80c6844
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2024-06-02 13:49:58 +0530

    minor code refactor

--- src/feature/home/index.js
+++ src/feature/home/index.js
@@ -2,20 +2,26 @@ import { useEffect } from "react";
 import TourSearch from "./TourSearch";
 import { fetchCities } from "./redux/thunk";
 import { useDispatch, useSelector } from "react-redux";
+import { Button } from "antd";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const apiStatus = useSelector(state => state.search.apiStatus);
-  
-  useEffect(() => {
-    dispatch(fetchCities)
-  }, [dispatch])
+  const apiStatus = useSelector((state) => state.search.apiStatus);
 
-  if(apiStatus === "init" || apiStatus === "pending") {
-    return <h1>Loading ..</h1>
+  const onLoadPage = () => {
+    dispatch(fetchCities);
+  };
+
+  useEffect(() => {
+    onLoadPage();
+  }, []);
+
+  if (apiStatus === "init" || apiStatus === "pending") {
+    return <h1>Loading ..</h1>;
   }
 
-  if(apiStatus === "error") return <p>Error</p>
+  if (apiStatus === "error")
+    return <Button onClick={onLoadPage}>Reload Page</Button>;
 
   return (
     <>
