463a8726874582f611fb4c102d202079b06b6f65
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2024-06-02 14:23:04 +0530

    booking endpoint changed

--- src/network/endpoints.js
+++ src/network/endpoints.js
@@ -3,8 +3,8 @@ const baseUrl = "http://localhost:8080";
 const Endpoints = {
   login: "auth/login",
   signup: "auth/signup",
-  book: "book",
-  cities:`${baseUrl}/cities`,
+  book: "ticket/book",
+  cities: `${baseUrl}/cities`,
 };
 
 export default Endpoints;
